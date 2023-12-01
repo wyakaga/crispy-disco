79dbc0041de601f5ff3e7396f870f7f4318d3d99
wyakaga <123617005+wyakaga@users.noreply.github.com>
2023-12-01 19:48:04 +0700

    [FEAT] Add testing for getDetail

--- test/api.test.js
+++ test/api.test.js
@@ -32,3 +32,22 @@ describe('API test', () => {
       });
   });
 });
+
+describe('API test', () => {
+  it('should return status: success & data from /api/user/:id endpoint', (done) => {
+    request
+      .get('/api/user/1')
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.status).to.equal('success');
+        expect(res.body.data.id).to.be.a('number');
+        expect(res.body.data.id).to.equal(1);
+        expect(res.body.data.name).to.be.a('string');
+        expect(res.body.data.name).to.equal('Jhon');
+        expect(res.body.data.name).to.be.a('string');
+        expect(res.body.data.email).to.equal('jhon@gmail.com');
+        return done();
+      });
+  });
+});
